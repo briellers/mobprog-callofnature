cbe560078690ed7dec82d56a6c8825e6268fdde5
brie <57290941+briellers@users.noreply.github.com>
2020-03-01 21:59:14 +0800

    Button functions on dashboard

--- app/src/main/java/com/example/callofnature/DashboardActivity.java
+++ app/src/main/java/com/example/callofnature/DashboardActivity.java
@@ -1,14 +1,36 @@
 package com.example.callofnature;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    Button yes, no;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        yes = findViewById(R.id.yes);
+        no = findViewById(R.id.no);
+
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DashboardActivity.this, "You clicked yes", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DashboardActivity.this, "You clicked no", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
