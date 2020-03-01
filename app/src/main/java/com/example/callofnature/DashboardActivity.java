adbcc2e6712599a4ca312cfa78ff3e378b04f39b
brie <57290941+briellers@users.noreply.github.com>
2020-03-01 21:51:17 +0800

    Added dashboard

---
+++ app/src/main/java/com/example/callofnature/DashboardActivity.java
@@ -0,0 +1,14 @@
+package com.example.callofnature;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+}
